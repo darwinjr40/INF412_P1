18d06b2c1e8acd93ba3283dd8285eb4724738675
Darwin Mamani <darwin_xd10@hotmail.com>
2022-04-27 02:51:07 -0400

    v09

--- public/js/inquiry.js
+++ public/js/inquiry.js
@@ -14,12 +14,12 @@ function onSelectProjectChange(){
     // $.get('/clinica/public/doctor/'+speciality_id, function(data){
     $.get(' http://193.123.99.38/public/doctor/1', function(data){
 
-        console.log(data);  
-        // var xd = '<option value="">Seleccione un Doctor</option>';
-        // for (var i = 0; i < data.length; i++) {
-        //     xd += '<option value="' +data[i].doctor_id+ '">' + data[i].doctor_nombre+ '</option>'
-        // }
-        // //  console.log(xd);
-        // $('#select-doctor').html(xd);
+        // console.log(data);  
+        var xd = '<option value="">Seleccione un Doctor</option>';
+        for (var i = 0; i < data.length; i++) {
+            xd += '<option value="' +data[i].doctor_id+ '">' + data[i].doctor_nombre+ '</option>'
+        }
+        //  console.log(xd);
+        $('#select-doctor').html(xd);
     });
 }
